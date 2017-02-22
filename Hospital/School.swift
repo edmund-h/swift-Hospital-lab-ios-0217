c848382c0c9df3934de58961757ad0637f210d7d
edmund-h <447ght@gmail.com>
2017-02-21 22:31:24 -0500

    i love protocols

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -2,12 +2,70 @@
 //  School.swift
 //  Hospital
 
+protocol Employee: Payable, TimeOff{}
 
+protocol Payable {}
+extension Payable {
+    func wages()-> Double{
+        return 50_000
+    }
+}
 
+protocol TimeOff {
+    var vacationDays: Int {get set}
+}
+extension TimeOff {
+    func requestForVacation (_ amount: Int)-> Bool {
+        guard amount <= vacationDays
+            || (amount == 9 && vacationDays == 10) else {return false}
+        return true
+    }
+}
 
+enum Punishment {
+    case severe, medium, light
+}
 
+protocol Reprimand {}
+extension Reprimand{
+    func disciplineStudent(_ scale: Punishment)-> String{
+    switch scale{
+        case .severe:
+            return "We are expelling you from the school!"
+        case .light:
+            return "You're to go back to class, don't do it again."
+        case .medium:
+            return "Why would you think that's a good idea?! You're going to detention."
+    }
+}
+    func callParentDeliveringMessage(_ scale: Punishment)-> String{
+        switch scale {
+        case .severe:
+            return "Your child has been expelled from the school."
+        case .light:
+            return "Your child thought it was funny to put gum in Amy's hair."
+        case .medium:
+            return "Your child has become a terror in the classroom."
+        }
+    }
+}
 
+enum Subject{
+    case math, science, english
+}
 
+protocol Teach{}
+extension Teach{
+    func teachSubject (_ subject: Subject)-> String{
+        switch subject{
+        case .math:
+            return "Take out your math books please."
+        case .science:
+            return "Time to learn the best subject of all! Science!!"
+        case .english:
+            return "To read or not to read. Everyone take out your english books."        }
+    }
+}
 
 
 
--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,9 +3,44 @@
 //  Hospital
 
 
+class AdvisoryBoardMember: Employee{
+    let name: String
+    var vacationDays: Int
+    
+    init (name: String, vacationDays: Int) {
+        self.name = name
+        self.vacationDays = vacationDays
+    }
+    
+    func wages() -> Double {
+        return 100_000
+    }
+}
 
+class Principal: Employee, Reprimand{
+    let name: String
+    var vacationDays: Int
+    
+    init (name: String, vacationDays: Int) {
+        self.name = name
+        self.vacationDays = vacationDays
+    }
+    
+    func wages() -> Double {
+        return 80_000
+    }
+}
 
-
+class Teacher: Employee, Teach{
+    let name: String
+    var vacationDays: Int
+    
+    init (name: String, vacationDays: Int){
+        self.name = name
+        self.vacationDays = vacationDays
+    }
+    
+}
 
 
 
